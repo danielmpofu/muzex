3947956f45ce927d5ee935cec9e1e2ea6f73b2b8
danielmpofu <danielmpofu@hotmail.com>
2022-10-04 16:35:04 +0200

    first commit

---
+++ wp-content/themes/muzex/assets/css/color-1.php
@@ -0,0 +1,64 @@
+/*========== Color ====================*/
+
+:root {
+	--thm-base: #aaa081!important;
+}
+
+
+.block-title-two p,
+.funfact-section .counter-inner .counter-box,
+.gallery-ex-section .post-filter li:hover, .gallery-ex-section .post-filter li.active,
+.footer-ex-section .footer-widget p a,
+.footer-ex-section .footer-widget__links-list li a:hover,
+.feature-block-one .inner-box .icon-box
+
+{
+	color:#aaa081!important;
+}
+
+/*==============================================
+   Theme Background Css
+===============================================*/
+
+.btn-style-two,
+.gallery-ex-section .post-filter li:before,
+.team-ex-section .team-one__image .social-links li a:hover,
+.feature-block-one .inner-box:hover a,
+.mrsidebar .widget-title h3:before,
+.tagcloud a:hover,
+.sidebar-widget li::before
+
+{
+	background: #aaa081!important;
+	background-color:#aaa081!important;
+}
+
+
+/*==============================================
+   Theme Border Color Css
+===============================================*/
+
+
+.post-social-link ul li a:hover,
+.team-ex-section .team-one__image .social-links li a:hover,
+.testimonials-one__carousel.owl-theme .owl-nav [class*=owl-],
+.tagcloud a:hover
+
+{
+    border-color: #aaa081!important;  
+}
+
+/*==============================================
+   RGB
+===============================================*/
+
+.video-galler-outer-bg:before {
+    background-color: rgba(170,160,129,0.9)!important;
+}
+.main-slider .content h3:before{
+    background: rgba(170,160,129,0.4)!important;
+}
+
+
+
+
